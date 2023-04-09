258ed4303731124760574e438b2f5dffb95d8dce
Shahi77 <shahiinfinity02@gmail.com>
2023-04-09 19:46:31 +0530

    added navigation bar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/styles';
+
 
 import App from './components/App';
 
+const theme = createTheme({});
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-
+    <ThemeProvider theme={theme}>,
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>,
+    </ThemeProvider>,
     document.getElementById('root')
 );
