dbdc8302e7bef52cfcea9916c98357e3f298bf72
Shahi77 <shahiinfinity02@gmail.com>
2023-04-16 23:47:11 +0530

    redux setup

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import { BrowserRouter } from 'react-router-dom';
 import { createTheme } from '@mui/material/styles';
 import { ThemeProvider } from '@mui/styles';
 
-
 import App from './components/App';
+import { Provider } from 'react-redux';
 
 const theme = createTheme({});
 ReactDOM.render(
-    <ThemeProvider theme={theme}>,
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>,
-    </ThemeProvider>,
+    <Provider store={store}>
+        <ThemeProvider theme={theme}>,
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>,
+        </ThemeProvider>,
+    </Provider>,
     document.getElementById('root')
 );
